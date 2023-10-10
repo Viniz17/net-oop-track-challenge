13ebb7c7f27818a8073872cf7b92e872b9c83a50
Vinicius Viana Deniz <vinicius.deniz@t2mlab.com>
2023-10-10 19:39:15 -0300

    feat(Models): add new classes and methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Realizando testes com Nokia:");
+Smartphone nokia = new Nokia(numero: "123", modelo: "Nokia 3310", imei: "IMEI123", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+Console.WriteLine("\n");
+
+
+
+Console.WriteLine("Realizando testes com Iphone:");
+Smartphone iphone = new Iphone(numero: "456", modelo: "Iphone 13", imei: "IMEI456", memoria: 128);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
